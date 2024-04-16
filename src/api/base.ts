a38d73f88eec2cbfa1da0f095115a75c016f2ece
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2024-04-16 22:57:24 +0300

    fix: Update env names

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const getBaseAPIUrl = () => {
-    return process.env.NODE_ENV === "development"
+    return process.env.NEXT_PUBLIC_NODE_ENV === "development"
         ? "http://127.0.0.1:8000"
-        : process.env.API_URL;
+        : process.env.NEXT_PUBLIC_API_URL;
 };
 
 const instance = axios.create({
