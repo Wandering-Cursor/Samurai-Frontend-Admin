44686f68675f4a764ac636f3970fe14392ba100d
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2024-04-23 13:47:43 +0300

    fix: Small visual changes to the pages

--- src/components/SidePanel/SidePanel.vue
+++ src/components/SidePanel/SidePanel.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="card flex justify-content-center w-full">
         <Menu :model="items" class="flex-grow-1">
+            <template #start>
+                <span class="flex align-items-center justify-content-center gap-1 px-1 py-2">
+                    <span class="pi pi-graduation-cap text-2xl text-primary"></span>
+                    <span class="font-medium text-2xl font-semibold">Admin Page</span>
+                </span>
+                <Divider />
+            </template>
             <template #item="{ item, props }">
                 <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
                     <a v-ripple :href="href" v-bind="props.action" @click="navigate">
@@ -8,16 +15,23 @@
                         <span class="ml-2">{{ item.label }}</span>
                     </a>
                 </router-link>
-                <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
-                    <span :class="item.icon" />
-                    <span class="ml-2">{{ item.label }}</span>
-                </a>
+            </template>
+            <template #end>
+                <Divider />
+                <span class="flex align-items-center justify-content-center">
+                    <span class="font-medium text-xs font-light">By
+                        <a href="https://github.com/Wandering-Cursor/">
+                            <Button link label="Wandering-Cursor" class="font-medium text-xs font-light p-0 m-0" />
+                        </a>
+                        team</span>
+                </span>
             </template>
         </Menu>
     </div>
 </template>
 
 <script setup lang="ts">
+import Divider from 'primevue/divider';
 import { ref } from 'vue';
 
 const items = ref([
