289879e39097721b290a909b4845b68e36b77d6d
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2024-05-02 18:02:20 +0300

    fix: Return WebHistory, use catchall => /

--- src/pages/router.ts
+++ src/pages/router.ts
@@ -1,5 +1,5 @@
 import { createRouter } from "vue-router";
-import { createWebHashHistory } from "vue-router";
+import { createWebHistory } from "vue-router";
 
 const routes = [
   {
@@ -47,11 +47,15 @@ const routes = [
       },
     ],
   },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: "/",
+  },
 ];
 
 export default function () {
   return createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes: routes,
   });
 }
