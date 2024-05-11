ea2e42b7ab699f1cb02308369c506233a3f7eef4
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2024-05-11 10:52:24 +0300

    refactor

--- src/components/Department/DepartmentTable.vue
+++ src/components/Department/DepartmentTable.vue
@@ -2,30 +2,47 @@
 import { Meta } from '@/api/types/common/Pagination';
 import CopyToClipboard from "@/components/Visuals/CopyToClipboard.vue";
 import { PageState } from 'primevue/paginator';
-import { onMounted, ref } from 'vue';
+import { Ref, onMounted, reactive, ref } from 'vue';
 import { AxiosError } from 'axios';
-import { defineProps } from 'vue';
 import { useToast } from 'primevue/usetoast';
 import { Department, DepartmentSearchRequest, DepartmentSearchResponse } from '@/api/types/organization/Department';
 import { searchDepartments } from '@/api/organization/department/searchDepartments';
 import { deleteDepartment } from '@/api/organization/department/deleteDepartment';
+import { SearchFilter } from '@/api/types/common/Search';
+import FilterPanel from "@/components/Search/FilterPanel/FilterPanel.vue";
 
 const toast = useToast();
 
-const props = defineProps<{
-    list: Department[];
-    metaInfo: Meta;
-    pageFilters: DepartmentSearchRequest;
-    changePageCallback: (event: PageState) => void;
-}>();
+const list = reactive(new Array<Department>());
+let metaInfo: Ref<Meta> = ref(new Meta());
+let pageFilters: Ref<DepartmentSearchRequest> = ref(new DepartmentSearchRequest(1, 10));
 
 const deleteDialogVisible = ref(false);
 const deleteDepartmentID = ref('');
 
+const filters: SearchFilter[] = [
+    {
+        title: "Name",
+        propertyName: "name",
+        placeholder: "КІПтаКЗ",
+    },
+]
+
 const gotDepartments = (data: DepartmentSearchResponse) => {
-    props.list.splice(0, props.list.length);
-    props.list.push(...data.content);
-    props.metaInfo.fromMeta(data.meta);
+    list.splice(0, list.length);
+    list.push(...data.content);
+    metaInfo.value.fromMeta(data.meta);
+};
+
+const onFilter = (filters: { [key: string]: any }) => {
+    pageFilters.value = new DepartmentSearchRequest(pageFilters.value.page, pageFilters.value.page_size, filters.name);
+    searchDepartments(pageFilters.value, gotDepartments, showError);
+};
+
+const onPageChange = (event: PageState) => {
+    pageFilters.value.page = event.page + 1;
+    pageFilters.value.page_size = event.rows;
+    onFilter(pageFilters.value);
 };
 
 const showError = (error: AxiosError) => {
@@ -34,7 +51,7 @@ const showError = (error: AxiosError) => {
 }
 
 onMounted(() => {
-    searchDepartments(props.pageFilters, gotDepartments, showError);
+    searchDepartments(pageFilters.value, gotDepartments, showError);
 });
 
 const showDeleteDepartmentDialog = (faculty_id: string) => {
@@ -47,7 +64,7 @@ const deleteDepartmentAction = () => {
         deleteDepartmentID.value,
         () => {
             toast.add({ severity: 'success', summary: 'Success', detail: 'Department deleted successfully', life: 3000 });
-            searchDepartments(props.pageFilters, gotDepartments, showError);
+            searchDepartments(pageFilters.value, gotDepartments, showError);
             deleteDialogVisible.value = false;
         },
         (error: AxiosError) => {
@@ -61,41 +78,52 @@ const deleteDepartmentAction = () => {
 
 
 <template>
-    <DataTable :value="list" showGridlines stripedRows>
-        <template #header>
-            <div class="flex flex-wrap align-items-center justify-content-between gap-2">
-                <div></div>
-                <RouterLink to="/organization/departments/create">
-                    <Button icon="pi pi-plus" v-tooltip="'Create new account'" label="New" raised link />
-                </RouterLink>
-            </div>
-        </template>
-        <Column key="department_id" field="department_id" header="Department ID" />
-        <Column key="name" field="name" header="Name" />
-        <Column key="description" field="description" header="Description" />
-        <Column key="faculties_count" field="faculties_count" header="Faculties Count" />
-        <Column key="created_at" field="created_at" header="Created At" />
-        <Column key="updated_at" field="updated_at" header="Updated At" />
-        <Column key="actions" header="Actions">
-            <template #body="slotProps">
-                <div class="flex flex-wrap gap-2">
-                    <CopyToClipboard :data="(slotProps.data as Department).department_id" tooltip="Copy ID"
-                        icon="pi pi-copy" />
-                    <RouterLink :to="`/organization/departments/${(slotProps.data as Department).department_id}/edit`">
-                        <Button icon="pi pi-pencil" v-tooltip="'Edit Department'" />
-                    </RouterLink>
-                    <Button icon="pi pi-trash" v-tooltip="'Delete Department'" class="p-button-danger"
-                        @click="showDeleteDepartmentDialog((slotProps.data as Department).department_id as string)" />
-                </div>
-            </template>
-        </Column>
-    </DataTable>
-    <Paginator :rows="$props.metaInfo.page_size" :totalRecords="$props.metaInfo.total" v-on:page="changePageCallback"
-        template="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown CurrentPageReport"
-        :rowsPerPageOptions="[1, 5, 10, 20, 30]" />
+    <div class="grid w-full">
+        <div class="col-10 pt-4">
+
+            <DataTable :value="list" showGridlines stripedRows>
+                <template #header>
+                    <div class="flex flex-wrap align-items-center justify-content-between gap-2">
+                        <div></div>
+                        <RouterLink to="/organization/departments/create">
+                            <Button icon="pi pi-plus" v-tooltip="'Create new account'" label="New" raised link />
+                        </RouterLink>
+                    </div>
+                </template>
+                <Column key="department_id" field="department_id" header="Department ID" />
+                <Column key="name" field="name" header="Name" />
+                <Column key="description" field="description" header="Description" />
+                <Column key="faculties_count" field="faculties_count" header="Faculties Count" />
+                <Column key="created_at" field="created_at" header="Created At" />
+                <Column key="updated_at" field="updated_at" header="Updated At" />
+                <Column key="actions" header="Actions">
+                    <template #body="slotProps">
+                        <div class="flex flex-wrap gap-2">
+                            <CopyToClipboard :data="(slotProps.data as Department).department_id" tooltip="Copy ID"
+                                icon="pi pi-copy" />
+                            <RouterLink
+                                :to="`/organization/departments/${(slotProps.data as Department).department_id}/edit`">
+                                <Button icon="pi pi-pencil" v-tooltip="'Edit Department'" />
+                            </RouterLink>
+                            <Button icon="pi pi-trash" v-tooltip="'Delete Department'" class="p-button-danger"
+                                @click="showDeleteDepartmentDialog((slotProps.data as Department).department_id as string)" />
+                        </div>
+                    </template>
+                </Column>
+            </DataTable>
+            <Paginator :rows="metaInfo.page_size" :totalRecords="metaInfo.total" v-on:page="onPageChange"
+                template="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown CurrentPageReport"
+                :rowsPerPageOptions="[1, 5, 10, 20, 30]" />
+        </div>
+        <div class="col-2">
+            <FilterPanel :filters="filters" :filterHandler="onFilter" search-name="Departments" />
+        </div>
+    </div>
+
 
     <Dialog v-model:visible="deleteDialogVisible" modal header="Delete faculty" :style="{ width: '50%' }">
-        <span class="p-text-secondary block mb-5">Are you sure you want to delete department {{ deleteDepartmentID
+        <span class="p-text-secondary block mb-5">Are you sure you want to delete department {{
+            deleteDepartmentID
             }}?</span>
         <div class="flex flex-row gap-2 justify-content-center">
             <Button label="Yes" @click="deleteDepartmentAction" />
--- src/pages/Main/Organization/Department/DepartmentList.vue
+++ src/pages/Main/Organization/Department/DepartmentList.vue
@@ -1,49 +1,8 @@
 <script setup lang="ts">
-import FilterPanel from "@/components/Search/FilterPanel/FilterPanel.vue";
 
-import { searchDepartments } from "@/api/organization/department/searchDepartments";
-
-import { AxiosError } from "axios";
-
-import { Ref, ref } from "vue";
-
-import { Meta } from "@/api/types/common/Pagination";
-import { SearchFilter } from "@/api/types/common/Search";
-import { Department, DepartmentSearchRequest, DepartmentSearchResponse } from "@/api/types/organization/Department";
 import DepartmentTable from "@/components/Department/DepartmentTable.vue";
 
-let list: Ref<Array<Department>> = ref([]);
-let metaInfo: Ref<Meta> = ref(new Meta());
-let pageFilters: Ref<DepartmentSearchRequest> = ref(new DepartmentSearchRequest(1, 10));
 
-const showError = (error: AxiosError) => {
-    console.error(error);
-}
-
-const gotDepartments = (data: DepartmentSearchResponse) => {
-    list.value = [];
-    list.value.push(...data.content);
-    metaInfo.value.fromMeta(data.meta);
-};
-
-const onFilter = (filters: { [key: string]: any }) => {
-    pageFilters.value = new DepartmentSearchRequest(pageFilters.value.page, pageFilters.value.page_size, filters.name);
-    searchDepartments(pageFilters.value, gotDepartments, showError);
-};
-
-const onPageChange = (event: { page: number, rows: number }) => {
-    pageFilters.value.page = event.page + 1;
-    pageFilters.value.page_size = event.rows;
-    onFilter(pageFilters.value);
-};
-
-const filters: SearchFilter[] = [
-    {
-        title: "Name",
-        propertyName: "name",
-        placeholder: "КІПтаКЗ",
-    },
-]
 
 </script>
 
@@ -53,14 +12,7 @@ const filters: SearchFilter[] = [
             <p class="text-4xl font-bold text-center">Departments Control</p>
         </div>
         <div class="grid w-full">
-            <div class="col-10 pt-4">
-                <DepartmentTable :list="list" :metaInfo="metaInfo" :pageFilters="pageFilters"
-                    :change-page-callback="onPageChange">
-                </DepartmentTable>
-            </div>
-            <div class="col-2">
-                <FilterPanel :filters="filters" :filterHandler="onFilter" search-name="Departments" />
-            </div>
+            <DepartmentTable />
         </div>
     </div>
 </template>
