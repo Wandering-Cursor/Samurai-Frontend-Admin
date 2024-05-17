4fdaf59e1f93fefee927b82767b798ca0e34619f
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2024-05-17 17:46:20 +0300

    Make both env variables required

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string;
-  readonly VITE_API_URL: string?;
+  readonly VITE_APP_TITLE: string = 'Vite App';
+  readonly VITE_API_URL: string = 'http://localhost:8000';
 }
 
 interface ImportMeta {
